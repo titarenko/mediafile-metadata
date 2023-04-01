dd7bed7ce7202ccaca9f5e4cfbdc693c3eb267f7
Constantin Titarenko <constantin.titarenko@gmail.com>
2023-04-01 21:47:19 +0300

    dry test

--- __tests__/isobmff.ts
+++ __tests__/isobmff.ts
@@ -2,24 +2,24 @@ import { describe, expect, test } from "@jest/globals";
 import fs from "fs/promises";
 import { FileReader } from "../src/reader";
 import { parseEssentials } from "../src/isobmff";
+import { Essentials } from "../src/essentials";
 
 describe("isobmff", () => {
-  test("isom", async () => {
-    const file = await fs.open(`${__dirname}/assets/isom.bin`);
-    const reader = new FileReader(file);
-    const essentials = await parseEssentials(reader);
-    expect(essentials).toStrictEqual({
-      creationDate: new Date("2023-03-25T15:34:24.000Z"),
-    });
+  t("isom", {
+    creationDate: new Date("2023-03-25T15:34:24.000Z"),
   });
 
-  test("heic", async () => {
-    const file = await fs.open(`${__dirname}/assets/heic.bin`);
-    const reader = new FileReader(file);
-    const essentials = await parseEssentials(reader);
-    expect(essentials).toStrictEqual({
-      creationDate: new Date("2022-05-10T15:48:20.000Z"),
-      camera: "Apple iPhone XS",
-    });
+  t("heic", {
+    creationDate: new Date("2022-05-10T15:48:20.000Z"),
+    camera: "Apple iPhone XS",
   });
 });
+
+function t(testName: string, expectedResult: Essentials | undefined) {
+  test(testName, async () => {
+    const file = await fs.open(`${__dirname}/assets/${testName}.bin`);
+    const reader = new FileReader(file);
+    const essentials = await parseEssentials(reader);
+    expect(essentials).toStrictEqual(expectedResult);
+  });
+}
